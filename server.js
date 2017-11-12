1bb6f7559adc5e312197eb557b914f18b9d880d6
Franklin Chieze <chieze.franklin@gmail.com>
2017-11-12 01:13:02 +0100

    Minor changes

--- server.js
+++ server.js
@@ -13,34 +13,46 @@ app.post('/', (req, res) => {
 
     soClient(req.body.text)
     .then((response) => {
-      const items = response.items; 
-      if (items.length > 100) {//slack does not allow more than 100 attachments
-        items = items.splice(100);
-      }
-      let attachments = items.map((item) => {
-        return {
-          color: 'good',
-          title: item.title,
-          title_link: item.link,
-          text: item.link
-        };
-      });
-      if (items.length === 0) {
-        var text = req.body.text.replace(' ', '+'); //replace spaces with pluses to form part of the google url
-        var googleLink = 'https://www.google.com/search?q=' + text;
-        
+      let attachments = [];
+
+      if (response.constructor === Error) {
         attachments = [{
           color: 'danger',
-          title: 'No result found!',
-          text: 'Consider rephrasing your request. For instance, make the request shorter by using only key words.'
-        }, {
-          color: 'warning',
-          title: 'Consider asking google',
-          title_link: googleLink,
-          text: googleLink
+          text: 'An error occured while trying to retreive a response from Stack Overflow'
         }];
       }
+      else if (response.items) {
+        const items = response.items;
 
+        if (items.length === 0) {
+          var text = req.body.text.replace(' ', '+'); //replace spaces with pluses to form part of the google url
+          var googleLink = 'https://www.google.com/search?q=' + text;
+          
+          attachments = [{
+            color: 'danger',
+            title: 'No result found!',
+            text: 'Consider rephrasing your request. For instance, make the request shorter by using only key words.'
+          }, {
+            color: 'warning',
+            title: 'Consider asking google',
+            title_link: googleLink,
+            text: googleLink
+          }];
+        }
+        else if (items.length > 100) {//slack does not allow more than 100 attachments
+          items = items.splice(100);
+        }
+
+        attachments = items.map((item) => {
+          return {
+            color: 'good',
+            title: item.title,
+            title_link: item.link,
+            text: item.link
+          };
+        });
+      }
+      
       request({
         url: req.body.response_url,
         method: 'POST',
