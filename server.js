c3bf3c65611453841b168c49cd7a3cabe2a82f1c
Franklin Chieze <chieze.franklin@gmail.com>
2018-08-17 21:04:03 +0100

    add hack to prevent app from sleeping

--- server.js
+++ server.js
@@ -80,3 +80,13 @@ let server = app.listen(process.env.PORT || 5000, () => {
   let port = server.address().port;
   console.log(`Server started on port ${port}`)
 })
+
+// little hack to prevent app from sleeping on heroku
+// https://quickleft.com/blog/6-easy-ways-to-prevent-your-heroku-node-app-from-sleeping/
+if (process.env.NODE_ENV === 'production') {
+  const https = require("https");
+  setInterval(function() {
+    https.get("https://stack-on-slack.herokuapp.com");
+    // console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>interval fired!!!");
+  }, 300000); // every 5 minutes
+}
